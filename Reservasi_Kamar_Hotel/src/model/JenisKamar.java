42671acbb53442729c805d1aaf293446852c84ba
Advent <161547416+Zendrato12@users.noreply.github.com>
2025-06-24 07:42:20 +0700

    Update JenisKamar.java

--- Reservasi_Kamar_Hotel/src/model/JenisKamar.java
+++ Reservasi_Kamar_Hotel/src/model/JenisKamar.java
@@ -25,7 +25,8 @@ public class JenisKamar {
     }
 
     public JenisKamar(int aInt, String string) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        this.id = id;
+        this.nama = nama;
     }
 
     // Getter dan Setter untuk ID
